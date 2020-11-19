c5cf88023960c52bb2d3601817a54995a2d62b49
nikhil14692 <pulelu.nikhil@gmail.com>
2020-11-19 15:44:51 +0530

    Designed PersonalDetails

---
+++ app/src/main/java/rutherfordit/com/instasalary/activities/PersonalDetails.java
@@ -0,0 +1,16 @@
+package rutherfordit.com.instasalary.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import rutherfordit.com.instasalary.R;
+
+public class PersonalDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_details);
+    }
+}
