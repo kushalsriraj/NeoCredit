deda44489456e4aee25058d6761b65a72013e7d5
nikhil14692 <pulelu.nikhil@gmail.com>
2020-11-27 13:02:07 +0530

    Designed Added to PrivateLimitedCompanyDetailsActivity, PrivateLimitedDirectorFirstDetailsActivity, PrivateLimitedDirectorSecondDetailsActivity

---
+++ app/src/main/java/rutherfordit/com/instasalary/activities/PrivateLimitedCompanyDetailsActivity.java
@@ -0,0 +1,16 @@
+package rutherfordit.com.instasalary.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import rutherfordit.com.instasalary.R;
+
+public class PrivateLimitedCompanyDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_private_limited_company_details);
+    }
+}
---
+++ app/src/main/java/rutherfordit/com/instasalary/activities/PrivateLimitedDirectorSecondDetailsActivity.java
@@ -0,0 +1,16 @@
+package rutherfordit.com.instasalary.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import rutherfordit.com.instasalary.R;
+
+public class PrivateLimitedDirectorSecondDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_private_limited_director_second_details);
+    }
+}
